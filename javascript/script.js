6bb5b67e531d0a2b3346a11ab7b0d5d7d003371a
artemDaineko <daineko.artem123@gmail.com>
2021-04-24 11:26:15 +0300

    commit(genres)

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,14 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict';
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,11 +18,42 @@ const personalMovieDB = {
     privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = +prompt('Один из последних просмотренных фильмов?', '');
-    let b = +prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
-console.log(personalMovieDB);
-
-
+rememberMyFilms();
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено слишком мало фильмов!');
+    } else if (personalMovieDB >= 10 && personalMovieDB < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Вы мне врете!');
+    }
+}
+detectPersonalLevel();
+function showMyDB() {
+    if (personalMovieDB.privat != true) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
